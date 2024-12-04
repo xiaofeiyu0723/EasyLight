4c2837a1e46ba634532b29ac6c92d98234f95340
YelloooBlue <YelloooBlue@outlook.com>
2024-12-04 21:38:54 +0800

    Add RF module integration and listener functionality with MQTT publishing support

--- mqtt/mqtt.ino
+++ mqtt/mqtt.ino
@@ -22,10 +22,83 @@
 
 */
 
+#include <RadioLib.h>
+#include <AceCRC.h>
+
 // #======================== Definitions ========================#
 
 #define MQTT_RECONNECT_DELAY 5000
 #define ROUTER_MAX_LEVELS 10
+
+// RF Pinout ========================
+/*
+  @@@ Caution @@@
+    Please check the pinout of your board，otherwise it may lead to the flash destroyed !!!
+    If it happens, you force to flash the bootloader to the board.
+    (Press and hold the BOOT button, then POWER ON again to enter burning mode)
+    Then, in the Arduino IDE, select the correct board and port, and burn the bootloader.
+*/
+
+/*
+// For ESP8266
+#define PIN_CS 15
+#define PIN_GDO0 5
+#define PIN_RST RADIOLIB_NC
+#define PIN_GDO2 4
+*/
+
+// For ESP32
+#define PIN_CS 5
+#define PIN_GDO0 21
+#define PIN_RST RADIOLIB_NC
+#define PIN_GDO2 22
+
+/*
+// For ESP32-C3
+#define PIN_CS 7
+#define PIN_GDO0 3
+#define PIN_RST RADIOLIB_NC
+#define PIN_GDO2 2
+*/
+
+// Pinout end ========================
+
+// RF Configurations
+#define RADIO_CARRIER_FREQUENCY 433.3   // MHz
+#define RADIO_BIT_RATE 250.0            // kbps
+#define RADIO_FREQUENCY_DEVIATION 125.0 // kHz
+#define RADIO_RX_BANDWIDTH 270.0        // kHz
+#define RADIO_OUTPUT_POWER 10           // dBm
+#define RADIO_PREAMBLE_LENGTH 32        // bits
+
+// Packet
+#define PACKET_RSSI_THRESHOLD -100  // dBm (RSSI threshold for packet reception)
+#define PACKET_LENGTH 20            // The Bytes you want to receive (after the sync word)
+uint8_t SYNC_WORD[] = {0x21, 0xA4}; // The [21 A4] would be better, [A4 23] is not working, I don't know why
+uint8_t SYNC_WORD_LENGTH = 2;
+#define PACKET_THIRD_SYNC_WORD 0x23       // or Payload Prefix (TODO: Implement the third sync word)
+using namespace ace_crc::crc16ccitt_byte; // Select the type of CRC algorithm we'll be using
+
+// Special
+volatile bool receivedFlag = false; // flag to indicate that a packet was received
+volatile bool transmitFlag = true;  // flag to indicate that a packet was transmitted
+#if defined(ESP8266) || defined(ESP32)
+ICACHE_RAM_ATTR // Place the function below in the RAM region
+#endif
+    void
+    setReceivedFlag(void) // void and no arguments
+{
+    receivedFlag = true; // we got a packet, set the flag
+}
+
+#if defined(ESP8266) || defined(ESP32)
+ICACHE_RAM_ATTR // Place the function below in the RAM region
+#endif
+    void
+    setTransmitFlag(void) // void and no arguments
+{
+    transmitFlag = true; // we got a packet, set the flag
+}
 
 // #======================== Prototypes ========================#
 
@@ -46,12 +119,47 @@ String macAddress_s; // MAC Address with out ':'
 
 cppQueue topicStack(sizeof(String), ROUTER_MAX_LEVELS, LIFO); // A stack of topics
 
+// Listener
+boolean listenerStarted = false;
+long listenerStartedAt = 0;
+long listenerDuration = 0;
+
+// RF
+CC1101 radio = new Module(PIN_CS, PIN_GDO0, PIN_RST, PIN_GDO2);
+
 // #======================== Initialization ========================#
 
 void setup()
 {
     Serial.begin(115200);
     delay(10);
+
+    // ===============================================
+
+    Serial.print(F("[CC1101] Initializing ... "));
+    int state = radio.begin(RADIO_CARRIER_FREQUENCY, RADIO_BIT_RATE, RADIO_FREQUENCY_DEVIATION, RADIO_RX_BANDWIDTH, RADIO_OUTPUT_POWER, RADIO_PREAMBLE_LENGTH);
+    radio.setCrcFiltering(false);
+    radio.fixedPacketLengthMode(PACKET_LENGTH);
+    radio.setSyncWord(SYNC_WORD, SYNC_WORD_LENGTH);
+    // Check Init
+    if (state == RADIOLIB_ERR_NONE)
+    {
+        Serial.println(F("success!"));
+    }
+    else
+    {
+        Serial.print(F("failed, code "));
+        Serial.println(state);
+        while (true)
+        {
+            delay(10);
+        }
+    }
+
+    radio.setPacketReceivedAction(setReceivedFlag); // Callback on packet received
+    radio.setPacketSentAction(setTransmitFlag);     // Callback on packet sent
+
+    // ===============================================
 
     Serial.print("[WiFi] Connecting to ");
     Serial.println(WIFI_SSID);
@@ -122,6 +230,69 @@ void setup()
 
 void loop()
 {
+
+    // RF
+    if (receivedFlag)
+    {
+        receivedFlag = false; // flag to indicate that a packet is being received
+
+        byte byteArr[PACKET_LENGTH];
+        int state = radio.readData(byteArr, PACKET_LENGTH);
+
+        // Print Content
+        Serial.print(F("Received packet: "));
+        for (uint8_t i = 0; i < PACKET_LENGTH; i++)
+        {
+            Serial.print(byteArr[i], HEX);
+            Serial.print(F(" "));
+        }
+        Serial.println();
+
+        // Publish Listener
+        if (listenerStarted && mqttClient.connected())
+        {
+            String topic = "easylight/" + macAddress_s + "/listener";
+            String packet_s = bytesToHexStr(byteArr, PACKET_LENGTH);
+            mqttClient.publish(topic.c_str(), packet_s.c_str());
+            Serial.print("[MQTT] Published to ");
+            Serial.print(topic);
+            Serial.print(" with payload: ");
+            Serial.println(packet_s);
+        }
+
+        // TODO: Process the packet
+
+        radio.startReceive(); // put module back to listen mode
+    }
+
+    // Listener
+    if (listenerStarted)
+    {
+        long now = millis();
+        if (now - listenerStartedAt > listenerDuration)
+        {
+            listenerStarted = false;
+            Serial.println("[RF] Listener Stopped!");
+
+            Serial.print(F("[CC1101] Stopping to listen ... "));
+            int state = radio.standby();
+            if (state == RADIOLIB_ERR_NONE)
+            {
+                Serial.println(F("success!"));
+            }
+            else
+            {
+                Serial.print(F("failed, code "));
+                Serial.println(state);
+            }
+
+            // Publish Listener
+            String topic = "easylight/" + macAddress_s + "/listener";
+            mqttClient.publish(topic.c_str(), "STOPPED");
+        }
+    }
+
+    // MQTT
     if (!mqttClient.connected())
     {
         long now = millis();
@@ -141,26 +312,6 @@ void loop()
 }
 
 // #======================== Functions ========================#
-void mqtt_callback(char *topic, byte *payload, unsigned int length)
-{
-    Serial.print("[MQTT] Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
-
-    // Payload 2 String
-    String payload_s = "";
-    for (int i = 0; i < length; i++)
-    {
-        payload_s += (char)payload[i];
-    }
-    Serial.println(payload_s);
-
-    // Parse Topic
-    int stackSize = parseMQTTTopic2stack(topic, &topicStack);
-
-    // Routering payload to the right place
-    messageRouter(&topicStack, payload_s);
-}
 
 boolean mqtt_reconnect()
 {
@@ -313,6 +464,58 @@ void messageRouter(cppQueue *topicStack, String payload)
             {
                 Serial.print("\t\tInvalid Controller Operation: ");
                 Serial.println(controller_param);
+                return;
+            }
+        }
+
+        else if (item == "listener" && bridge_id.length() == 12)
+        {
+            if (topicStack->isEmpty())
+            {
+                Serial.println("\t\tListener parameter is missing!");
+                return;
+            }
+
+            String listener_param;
+            topicStack->pop(&listener_param);
+
+            // do action
+            if (listener_param == "start")
+            {
+                Serial.println("\t\t[Starting] Listener"); // TAKE ACTION
+
+                // Get Duration
+                if (payload.length() == 0)
+                {
+                    Serial.println("\t\t\tListener Duration is missing!");
+                    return;
+                }
+
+                long duration = payload.toInt();
+                boolean started = startListener(duration);
+                if (!started)
+                {
+                    Serial.println("\t\t\tListener failed to start!");
+                    return;
+                }
+
+                // Publish Listener
+                String topic = "easylight/" + bridge_id + "/listener";
+                mqttClient.publish(topic.c_str(), "STARTED");
+                Serial.print("[MQTT] Published to ");
+                Serial.print(topic);
+                Serial.print(" with payload: ");
+                Serial.println("STARTED");
+
+                return;
+            }
+            // next level
+
+            // unknown
+            else
+            {
+                Serial.print("\t\tInvalid Listener Operation: ");
+                Serial.println(listener_param);
                 return;
             }
         }
@@ -494,6 +697,52 @@ void messageRouter(cppQueue *topicStack, String payload)
     }
 }
 
+boolean startListener(long duration)
+{
+    if (listenerStarted)
+    {
+        Serial.println("[RF] Listener is already started!");
+        return false;
+    }
+
+    listenerStarted = true;
+    listenerStartedAt = millis();
+    listenerDuration = duration;
+
+    Serial.print("[RF] Listener Started for ");
+    Serial.print(duration);
+    Serial.println("ms");
+
+    Serial.print(F("[CC1101] Starting to listen ... "));
+    int state = radio.startReceive();
+    return state == RADIOLIB_ERR_NONE;
+}
+
+// #======================== Callbacks ========================#
+
+void mqtt_callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("[MQTT] Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+
+    // Payload 2 String
+    String payload_s = "";
+    for (int i = 0; i < length; i++)
+    {
+        payload_s += (char)payload[i];
+    }
+    Serial.println(payload_s);
+
+    // Parse Topic
+    int stackSize = parseMQTTTopic2stack(topic, &topicStack);
+
+    // Routering payload to the right place
+    messageRouter(&topicStack, payload_s);
+}
+
+// #======================== Utilities ========================#
+
 int hexToBytes(const String &hex, byte *bytes)
 {
     int len = hex.length() / 2;
@@ -505,33 +754,53 @@ int hexToBytes(const String &hex, byte *bytes)
     return len;
 }
 
+String bytesToHexStr(byte *bytes, int len)
+{
+    String hex = "";
+    char buf[3];
+
+    for (int i = 0; i < len - 1; i++)
+    {
+        sprintf(buf, "%02X ", bytes[i]);
+        hex += buf;
+    }
+
+    sprintf(buf, "%02X", bytes[len - 1]);
+    hex += buf;
+
+    return hex;
+}
+
 /*
     // Subscribe
     easylight/C049EFCC9FFF/controller/add                           - PAYLOAD: 36F9FF
     easylight/C049EFCC9FFF/controller/remove                        - PAYLOAD: 36F9FF
+    easylight/C049EFCC9FFF/listener/start                           - PAYLOAD: Duration(s)
+
     easylight/C049EFCC9FFF/controller/36F9FF/reset                  - PAYLOAD: NULL
-    easylight/C049EFCC9FFF/controller/36F9FF/state/get              - PAYLOAD: NULL         (QUERY)      
+    easylight/C049EFCC9FFF/controller/36F9FF/state/get              - PAYLOAD: NULL         (QUERY)
     easylight/C049EFCC9FFF/controller/36F9FF/light/1/power/set      - PAYLOAD: ON/OFF
     easylight/C049EFCC9FFF/controller/36F9FF/light/1/power/get      - PAYLOAD: NULL         (QUERY)
     easylight/C049EFCC9FFF/controller/36F9FF/light/1/switch/add     - PAYLOAD: NULL
     easylight/C049EFCC9FFF/controller/36F9FF/light/1/switch/remove  - PAYLOAD: NULL
-    
+
     // Publish
     easylight/C049EFCC9FFF/controller/36F9FF/state                  - PAYLOAD: ONLINE/OFFLINE
-    easylight/C049EFCC9FFF/controller/36F9FF/light/1/power          - PAYLOAD: ON/OFF 
+    easylight/C049EFCC9FFF/controller/36F9FF/light/1/power          - PAYLOAD: ON/OFF
+    easylight/C049EFCC9FFF/listener                                 - PAYLOAD: The packet received from RF
 
 */
 
 /*
-// Possible values for client.state()
-#define MQTT_CONNECTION_TIMEOUT     -4
-#define MQTT_CONNECTION_LOST        -3
-#define MQTT_CONNECT_FAILED         -2
-#define MQTT_DISCONNECTED           -1
-#define MQTT_CONNECTED               0
-#define MQTT_CONNECT_BAD_PROTOCOL    1
-#define MQTT_CONNECT_BAD_CLIENT_ID   2
-#define MQTT_CONNECT_UNAVAILABLE     3
-#define MQTT_CONNECT_BAD_CREDENTIALS 4
-#define MQTT_CONNECT_UNAUTHORIZED    5
+    // Possible values for client.state()
+    #define MQTT_CONNECTION_TIMEOUT     -4
+    #define MQTT_CONNECTION_LOST        -3
+    #define MQTT_CONNECT_FAILED         -2
+    #define MQTT_DISCONNECTED           -1
+    #define MQTT_CONNECTED               0
+    #define MQTT_CONNECT_BAD_PROTOCOL    1
+    #define MQTT_CONNECT_BAD_CLIENT_ID   2
+    #define MQTT_CONNECT_UNAVAILABLE     3
+    #define MQTT_CONNECT_BAD_CREDENTIALS 4
+    #define MQTT_CONNECT_UNAUTHORIZED    5
 */
