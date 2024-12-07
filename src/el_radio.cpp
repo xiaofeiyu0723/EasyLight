e1fea2c1b936d1fef85a4d378c45df4bb82d24f1
YelloooBlue <YelloooBlue@outlook.com>
2024-12-07 13:42:48 +0800

    Add AceCRC library and enhance radio initialization with CRC support

--- src/el_radio.cpp
+++ src/el_radio.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
 #include <RadioLib.h>
+#include <AceCRC.h>
 
 // #======================== Definitions ========================#
 
+using namespace ace_crc::crc16ccitt_byte; // Select CRC algorithm
+
+// For ESP32 ONLY !!!
+#define PIN_CS 5
+#define PIN_GDO0 21
+#define PIN_RST RADIOLIB_NC
+#define PIN_GDO2 22
+
+// RF Configurations
+#define RADIO_CARRIER_FREQUENCY 433.3   // MHz
+#define RADIO_BIT_RATE 250.0            // kbps
+#define RADIO_FREQUENCY_DEVIATION 125.0 // kHz
+#define RADIO_RX_BANDWIDTH 270.0        // kHz
+#define RADIO_OUTPUT_POWER 10           // dBm
+#define RADIO_PREAMBLE_LENGTH 32        // bits
+
+// Packet
+#define PACKET_RSSI_THRESHOLD -100  // dBm (RSSI threshold for packet reception)
+#define PACKET_LENGTH 20            // The Bytes you want to receive (after the sync word)
+uint8_t SYNC_WORD[] = {0x21, 0xA4}; // The [21 A4] would be better, [A4 23] is not working, I don't know why
+uint8_t SYNC_WORD_LENGTH = 2;
+#define PACKET_THIRD_SYNC_WORD 0x23
+
 // #======================== Global Variables ========================#
 
+CC1101 Radio = new Module(PIN_CS, PIN_GDO0, PIN_RST, PIN_GDO2);
 Stream *Radio_Logger;
 bool radio_initialized = false;
+
+// Interrupts flags
+volatile bool receivedFlag = false; // flag to indicate that a packet was received
+volatile bool transmitFlag = true;  // flag to indicate that a packet was transmitted
 
 // #======================== Prototypes ========================#
 
@@ -20,10 +49,36 @@ int radio_setLoggerOutput(Stream *s);
 // private
 int radio_log_print(String message);
 
+// interrupt
+ICACHE_RAM_ATTR void setReceivedFlag(void);
+ICACHE_RAM_ATTR void setTransmitFlag(void);
+
 // #======================== Initialization ========================#
 
 int radio_init()
 {
+    radio_log_print("[Radio] Initializing ...\n");
+    int state = Radio.begin(RADIO_CARRIER_FREQUENCY, RADIO_BIT_RATE, RADIO_FREQUENCY_DEVIATION, RADIO_RX_BANDWIDTH, RADIO_OUTPUT_POWER, RADIO_PREAMBLE_LENGTH);
+    Radio.setCrcFiltering(false);
+    // Radio.fixedPacketLengthMode(PACKET_LENGTH);
+    Radio.setSyncWord(SYNC_WORD, SYNC_WORD_LENGTH);
+    if (state == RADIOLIB_ERR_NONE)
+    {
+        radio_log_print("[Radio] Initialized !\n");
+    }
+    else
+    {
+        radio_log_print("[Radio] initialization failed: ");
+        radio_log_print(String(state));
+        // while (true)
+        // {
+        //     delay(10);
+        // }
+    }
+
+    Radio.setPacketReceivedAction(setReceivedFlag); // Callback on packet received
+    Radio.setPacketSentAction(setTransmitFlag);     // Callback on packet sent
+
     radio_initialized = true;
     return 0;
 }
@@ -41,7 +96,6 @@ bool radio_isInitialized()
 {
     return radio_initialized;
 }
-
 
 int radio_setLoggerOutput(Stream *s)
 {
@@ -62,5 +116,15 @@ int radio_log_print(String message)
 // #======================== Callbacks ========================#
 
 // #======================== Interrupt ========================#
+
+ICACHE_RAM_ATTR void setReceivedFlag(void)
+{
+    receivedFlag = true;
+}
+
+ICACHE_RAM_ATTR void setTransmitFlag(void)
+{
+    transmitFlag = true;
+}
 
 // #======================== End ========================#
